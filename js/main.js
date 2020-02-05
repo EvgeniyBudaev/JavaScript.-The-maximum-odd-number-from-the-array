597f12b85932eae670254f0eedf03b5ce4403011
EvgeniyBudaev <budaev.e@gmail.com>
2020-02-05 08:26:55 +0300

    Максимальное нечетное число из массива

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
+// Максимальное нечетное число из массива
+// Arrow-function
+// Вариант №1
+const array = ['4', '3', '7', '10', '1'];
 
-		
-	// Максимальное нечетное число из массива
-	// Arrow-function
+const result = array
+	.map((el) => parseInt(el))
+	.filter((number) => number % 2)
+	.reduce((max, value) => Math.max(max, value), 0)
 
-	const array = ['4', '3', '7', '10', '1'];
+console.log(result);
 
-	const result = array
-		.map((el) => parseInt(el))
-		.filter((number) => number%2)
-		.reduce((max, value) => Math.max(max, value), 0)
-	
-		console.log(result);
-
-
-
-	
-	
+// Максимальное число из массива
+// Вариант №2
+const array2 = ['4', '3', '7', '10', '1'];
+const sumNumbersArray2 = Math.max(...array2);
+console.log(sumNumbersArray2);
